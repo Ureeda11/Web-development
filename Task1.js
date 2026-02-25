74b6b6e5177a138e99ac45ac2164dbcbd12d7e3f
Ureeda11 <ureedabibi2006@gmail.com>
2026-02-25 15:34:22 +0500

    Update name and age variables in Task1.js

--- Task1.js
+++ Task1.js
@@ -1,5 +1,6 @@
-let name = "Hassan"
-let age = 20
+let name = "Ureeda"
+let age = 19
 let isStudent = true
 
 console.log("My name is " + name+ ". I am " + age+ " years old. Student: "+isStudent);  
+
